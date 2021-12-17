9e0191a71d8659f0c4c68bf9a8e947bc04af7ae4
Lucas Oliveira <lucas.pereira_oli@hotmail.com>
2021-12-17 20:32:02 -0300

    first commit

---
+++ lib/home/home.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class Home extends StatefulWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      appBar: CupertinoNavigationBar(
+        middle: Text('Download'),
+      ),
+    );
+  }
+}
